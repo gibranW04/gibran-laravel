c733b99194ffd9c11cfccb492660ffe03ee89f20
Gibran Wicaksono <wicaksonogibran04@gmail.com>
2026-01-14 23:23:15 +0700

    update UI

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CheckoutController;
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/', function () {
+    return view('welcome');
+});
 
 
 Route::get('/product/{slug}', [HomeController::class, 'show'])
